7fbf22c2d0ef47892629989a5b6e4164ddeb2076
PauGoSi <127191401+PauGoSi@users.noreply.github.com>
2023-08-11 14:09:45 +0200

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+const laptopsElement = document.getElementById('laptops');
+
+const laptops = [];
+*/
+const getALoanButtonElement = document.getElementById('clc_loan_btn');
+
+getALoanButtonElement.addEventListener('click', function() {
+    window.prompt("Are you feeling lucky", "sure"); 
+})
+
